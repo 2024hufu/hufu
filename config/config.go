b3c781c875a160138409b1944a568d39ae09e328
sbsz <yishuiwang163@gmail.com>
2024-11-01 18:55:17 +0800

    更新配置

--- config/config.go
+++ config/config.go
@@ -2,8 +2,12 @@ package config
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/FISCO-BCOS/go-sdk/v3/client"
 	"gopkg.in/yaml.v2"
@@ -29,6 +33,11 @@ type Config struct {
 		TLSKeyFile  string `yaml:"tls_key_file"`
 		TLSCertFile string `yaml:"tls_cert_file"`
 	} `yaml:"fisco"`
+
+	Contract struct {
+		KeyShareContract string `yaml:"key_share_contract"`
+		DecisionContract string `yaml:"decision_contract"`
+	} `yaml:"contract"`
 }
 
 var GlobalConfig Config
@@ -66,4 +75,61 @@ func ReadConfig(pk string) *client.Config {
 	}
 
 	return config
+}
+
+type KeyData struct {
+	PublicKey  string `json:"publicKey"`
+	PrivateKey string `json:"privateKey"`
+}
+
+func ReadJuryKeys() ([]string, []string) {
+	juryPrivateKeys := []string{}
+	juryPublicKeys := []string{}
+
+	// Define the directory containing the key files
+	dir := "config/keys"
+
+	// Read all files in the directory
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	// Iterate over each file
+	for _, file := range files {
+		// Construct the full file path
+		filePath := filepath.Join(dir, file.Name())
+
+		// Open the file
+		jsonFile, err := os.Open(filePath)
+		if err != nil {
+			log.Printf("Failed to open file %s: %v", filePath, err)
+			continue
+		}
+
+		// Read the file contents
+		byteValue, err := io.ReadAll(jsonFile)
+		if err != nil {
+			log.Printf("Failed to read file %s: %v", filePath, err)
+			jsonFile.Close()
+			continue
+		}
+
+		// Close the file
+		jsonFile.Close()
+
+		// Parse the JSON
+		var keyData KeyData
+		err = json.Unmarshal(byteValue, &keyData)
+		if err != nil {
+			log.Printf("Failed to parse JSON from file %s: %v", filePath, err)
+			continue
+		}
+
+		// Append the keys to the respective slices
+		juryPrivateKeys = append(juryPrivateKeys, keyData.PrivateKey)
+		juryPublicKeys = append(juryPublicKeys, keyData.PublicKey)
+	}
+
+	return juryPrivateKeys, juryPublicKeys
 }
